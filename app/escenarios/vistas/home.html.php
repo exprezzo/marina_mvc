797ef483f69628f0ac4b29ad6246ee4ef564b487
colab_01.diem <runtim3.error@gmail.com>
2012-07-22 20:14:02 -0600

    NO SIRVE

--- app/escenarios/vistas/home.html.php
+++ app/escenarios/vistas/home.html.php
@@ -1,3 +1,8 @@
+<!--
+TODO
+Los sonidos solo han funcionado en Chrome,
+El aumentar y disminuir el tamaño de la imagen ocasiona problemas en Chrome
+-->
 <style type="text/css">
 .escena{
 	border-radius: 20px 20px 20px 20px;
@@ -24,14 +29,18 @@ border-radius: 25px;*/
 	position:absolute;
 }
 
-.obj_opciones{
+.opciones_wrapper{
 	background: none repeat scroll 0 0 black;
-	width: 40px;
-	height: 40px;
+	position:relative;
 -webkit-border-radius: 5px;
 -moz-border-radius: 5px;
 border-radius: 5px;
 	color:white;
+	margin-left:142px;
+	margin-top:30px;
+}
+.obj_opciones{
+	
 }
 
 .btn_audio:hover{
@@ -47,8 +56,13 @@ audio{
 <script type="text/javascript" src="http://localhost/ext-3.4.0/ext-all-debug.js"></script>
 <script type="text/javascript" src="/js/mootools/mootools-core-1.4.5-full-compat.js"></script>
 	<script type='text/javascript' >				
+		eucalia={
+			init:function(){
+			}
+		}
 		Ext.onReady(function(){
-			escena=Ext.select( 'div.escena');
+			eucalia.init();
+			/*escena=Ext.select( 'div.escena');
 			escena.on('click',function(){
 								
 					var id=Ext.get(arguments[1]).getAttribute('myid') ;
@@ -56,23 +70,17 @@ audio{
 					alert($numsJs['obj_'+id].principal);
 					alert($numsJs['obj_'+id].secundario);
 				},this,{delegate:'div.obj_escena'}			
-			);
+			);*/
 		});
 		
 		mostrarOpciones = function(objId){
 			//aumentar tamaño y modificar x,y para mantenre centrado
 			var imgId='obj_img_id_'+objId;			
-			
+			console.log("mostrar");
 			var imagen=Ext.get(imgId);
 			imagen.setX(imagen.getX()-16);
 			imagen.setY(imagen.getY()-10);			
 			imagen.setSize( imagen.getWidth() + 35 ,imagen.getHeight() + 35);
-			//var width=imagen.elements[0].getAttribute('width');
-			
-			//$(imgId).tween('left', "-5");
-			//$(imgId).tween('height', "160px");			
-			//$(imgId).tween('width', "160");
-			
 			
 			//Hacer visible la caja de opciones
 			var selector='obj_opciones_id_'+objId;			
@@ -82,7 +90,7 @@ audio{
 		}
 		
 		ocultarOpciones = function(objId){
-			
+			console.log("ocultar");
 			var selector='obj_opciones_id_'+objId;			
 			$(selector).tween('opacity', 0);
 			
@@ -90,12 +98,7 @@ audio{
 			var imagen=Ext.get(selector);
 			imagen.setX(imagen.getX()+16);
 			imagen.setY(imagen.getY()+10);
-			imagen.setSize( imagen.getWidth() - 35,imagen.getHeight() - 35);
-			//$(selector).tween('left', "0");
-			//$(selector).tween('height', "128px");
-			//$(selector).tween('width', "128px");
-			
-			
+			imagen.setSize( imagen.getWidth() - 33,imagen.getHeight() - 33);						
 		}
 		
 		reproducirTraduccion = function(id, tipo){						
@@ -118,19 +121,21 @@ audio{
 	<?php 		
 		foreach($this->objetos as $objeto){
 			echo 
-			'<div style=\'margin-left:'.$objeto['x'].'px; margin-top:'.$objeto['y'].'px;position:absolute;\'><div class="obj_escena" myid="'.$objeto['id'].'" ></div>			
-				<div style="margin-left:40px;" >
-					<img id="obj_img_id_'.$objeto['id'].'" onmouseout="ocultarOpciones('.$objeto['id'].')" onmouseover="mostrarOpciones('.$objeto['id'].')" style="float:left;" src='.RESOURCES_PATH.$objeto['ruta_imagen'].' alt="img" />
-					<table onmouseout="ocultarOpciones('.$objeto['id'].')" onmouseover="mostrarOpciones('.$objeto['id'].')" style="float:left;position:absolute;margin-top:30px;margin-left:120px;opacity:0;" class="obj_opciones" id="obj_opciones_id_'.$objeto['id'].'">
-						<tr class="primario">
-							<td >'.$objeto['sub_p'].'</td>
-							<td ><label class="btn_audio" onClick="reproducirTraduccion('.$objeto['id'].',\'p\')" ;><img src="'.RESOURCES_PATH.'imagenes/play.png"/></label></td>
-						</tr>
-						<tr class="secundario">
-							<td >'.$objeto['sub_s'].'</td>
-							<td><label class="btn_audio" onClick="reproducirTraduccion('.$objeto['id'].',\'s\')"><img src="'.RESOURCES_PATH.'imagenes/play.png"/></label></td>					
-						</tr>
-					</table>
+			'<div class="obj_wrap" style=\'margin-left:'.$objeto['x'].'px; margin-top:'.$objeto['y'].'px;position:absolute;\' >			
+				<div  class="objeto" onmouseout="ocultarOpciones('.$objeto['id'].')">
+					<img class="obj_img" id="obj_img_id_'.$objeto['id'].'"  onmouseover="mostrarOpciones('.$objeto['id'].')" style="float:left;" src='.RESOURCES_PATH.$objeto['ruta_imagen'].' alt="img" />
+					<div class="opciones_wrapper"  id="obj_opciones_id_'.$objeto['id'].'">
+						<table>
+							<tr class="primario">
+								<td style="background-color:white;">'.$objeto['sub_p'].'</td>
+								<td ><label class="btn_audio" onClick="reproducirTraduccion('.$objeto['id'].',\'p\');" ><img src="'.RESOURCES_PATH.'imagenes/play.png" /></label></td>
+							</tr>
+							<tr class="secundario">
+								<td style="background-color:white;">'.$objeto['sub_s'].'</td>
+								<td><label class="btn_audio" onClick="reproducirTraduccion('.$objeto['id'].',\'s\');"><img src="'.RESOURCES_PATH.'imagenes/play.png" /></label></td>					
+							</tr>
+						</table>
+					</div>
 					<div style="clear:both"></div>
 				</div>
 			</div>
